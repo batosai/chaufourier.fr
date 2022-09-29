df6f57d1308e8f6b7f8dd6dc80aec5cd083ade41
jeremy <jeremy@chaufourier.fr>
2022-09-30 00:30:36 +0200

    Update events.ts

--- start/events.ts
+++ start/events.ts
@@ -11,9 +11,10 @@ import Event from '@ioc:Adonis/Core/Event'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Logger from '@ioc:Adonis/Core/Logger'
 import Application from '@ioc:Adonis/Core/Application'
+
 Event.on('db:query', (query) => {
   if (Application.inProduction) {
-    Logger.debug(query)
+    Logger.debug(query.sql)
   } else {
     Database.prettyPrint(query)
   }
