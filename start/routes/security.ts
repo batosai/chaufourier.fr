4ee7f770e16b642c7879ff0810da8e8205b91092
jeremy <jeremy@chaufourier.fr>
2022-10-01 00:50:45 +0200

    Create security.ts

---
+++ start/routes/security.ts
@@ -0,0 +1,9 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('/', 'security/LoginController.create')
+    .as('security.login')
+    .middleware('guest')
+  Route.post('/', 'security/LoginController.store')
+    .middleware('guest')
+}).prefix('/security')
