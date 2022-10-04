f04341c223c8f778a3b3d8e97161cd85f1d0f988
jeremy <jeremy@chaufourier.fr>
2022-10-04 22:40:41 +0200

    Rename start/views.ts in star/view.ts

---
+++ start/view.ts
@@ -0,0 +1,16 @@
+/*
+|--------------------------------------------------------------------------
+| Preloaded File
+|--------------------------------------------------------------------------
+|
+| Any code written inside this file will be executed during the application
+| boot.
+|
+*/
+
+import View from '@ioc:Adonis/Core/View'
+import jrmc from '@jrmc/edge-components'
+
+View.use(jrmc)
+
+View.getRenderer()
--- start/views.ts
+++
@@ -1,6 +0,0 @@
-import View from '@ioc:Adonis/Core/View'
-import jrmc from '@jrmc/edge-components'
-
-View.use(jrmc)
-
-View.getRenderer()
