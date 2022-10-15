86db3b64fb3937ee08722ba1e628be159064382e
jeremy <jeremy@chaufourier.fr>
2022-10-15 23:40:01 +0200

    Create forgot.spec.ts

---
+++ tests/e2e/auth/forgot.spec.ts
@@ -0,0 +1,22 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { test } from '@japa/runner'
+
+test.group('Forgot password page', group => {
+
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('Should display error message if validation with empty fields', async ({ assert, page, route, getScreen }) => {
+    await page.goto(route('auth.password.create'))
+
+    let screen = await getScreen()
+
+    await page.locator('text=validate').click()
+
+    screen = await getScreen()
+
+    assert.exists(await screen.findByText(/required validation failed on email/))
+  })
+})
