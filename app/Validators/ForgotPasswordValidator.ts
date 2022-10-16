f8679534658182c61077edb548d30298039aa0ca
jeremy <jeremy@chaufourier.fr>
2022-10-16 23:15:36 +0200

    Update ForgotPasswordValidator.ts

--- app/Validators/ForgotPasswordValidator.ts
+++ app/Validators/ForgotPasswordValidator.ts
@@ -1,10 +1,13 @@
 import { schema } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { emailExistsRules } from './Rules/Email'
+import { rules } from '@ioc:Adonis/Core/Validator'
+
 export default class ForgotPasswordValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    email: schema.string(emailExistsRules()),
+    email: schema.string([
+      rules.email()
+    ]),
   })
 }
