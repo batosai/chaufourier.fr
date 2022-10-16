ce220e2cdd24708d364cc69e2ba0d8c1c5a3343f
jeremy <jeremy@chaufourier.fr>
2022-10-16 23:41:17 +0200

    Create forgotPassword.spec.ts
    Test email forgot password

---
+++ tests/functional/mailers/forgotPassword.spec.ts
@@ -0,0 +1,45 @@
+import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import I18n from '@ioc:Adonis/Addons/I18n'
+import UserFactory from 'Database/factories/UserFactory'
+import { assets } from 'Config/app'
+
+test.group('Forgot password mailer', group => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('exist email address', async ({ assert, client, route }) => {
+    const mailer = Mail.fake()
+    await client
+      .post(route('auth.password.store'))
+      .fields({
+        email: 'jeremy@adonis.com',
+      }).redirects(0).withCsrfToken()
+
+      assert.isFalse(mailer.exists(mail => {
+        return mail.subject === 'Welcome to AdonisJS'
+      }))
+
+    Mail.restore()
+  })
+
+  test('not exist email address', async ({ assert, client, route }) => {
+    const mailer = Mail.fake()
+    const user = await UserFactory.create()
+
+    await client
+      .post(route('auth.password.store'))
+      .fields({
+        email: user.email,
+      }).redirects(0).withCsrfToken()
+
+    assert.isTrue(mailer.exists({
+      subject: I18n.locale(I18n.defaultLocale).formatMessage('email.forgotPassword.subject')
+    }))
+
+    Mail.restore()
+  })
+})
