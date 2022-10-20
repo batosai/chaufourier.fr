80e5138cc1db977706a03044c34e04610354dacc
jeremy <jeremy@chaufourier.fr>
2022-10-20 22:59:29 +0200

    Add test success forgot password

--- tests/e2e/auth/forgotPassword.spec.ts
+++ tests/e2e/auth/forgotPassword.spec.ts
@@ -1,5 +1,7 @@
 import { test } from '@japa/runner'
 import Database from '@ioc:Adonis/Lucid/Database'
+import UserFactory from 'Database/factories/UserFactory'
+import I18n from '@ioc:Adonis/Addons/I18n'
 
 test.group('Forgot password page', group => {
 
@@ -11,12 +13,25 @@ test.group('Forgot password page', group => {
   test('Should display error message if validation with empty fields', async ({ assert, page, route, getScreen }) => {
     await page.goto(route('auth.password.create'))
 
-    let screen = await getScreen()
-
     await page.locator('text=validate').click()
 
-    screen = await getScreen()
+    let screen = await getScreen()
 
     assert.exists(await screen.findByText(/Email field is required/))
+  })
+
+  test('Should display success message', async ({ assert, page, route, getScreen }) => {
+    const user = await UserFactory.create()
+
+    await page.goto(route('auth.password.create'))
+
+    await page.getByLabel('Your email').fill(user.email)
+    await page.locator('text=validate').click()
+
+    let screen = await getScreen()
+
+    assert.exists(await screen.findByText(
+      I18n.locale(I18n.defaultLocale).formatMessage('form.success.forgotPassword')
+    ))
   })
 })
