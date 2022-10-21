0b786d9ae4364e39003ad7885a7ff3b5fcd660f8
jeremy <jeremy@chaufourier.fr>
2022-10-22 01:11:06 +0200

    Change color dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 const edgeComponent = require('@jrmc/edge-components/build/tailwind.config').default
+const dark = require("daisyui/src/colors/themes")["[data-theme=dark]"]
 
 module.exports = {
   mode: 'jit',
   content: ['./resources/views/**/*.edge', ...edgeComponent.content],
   plugins: [...edgeComponent.plugins],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...dark,
+          primary: dark.accent,
+          accent: dark.primary,
+        },
+      },
+    ],
+  },
 }
