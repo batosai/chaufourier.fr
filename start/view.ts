c38337211ffa2d89207252cd2a573057718461a4
jeremy <jeremy@chaufourier.fr>
2022-10-22 01:09:51 +0200

    Install and start iconify

--- start/view.ts
+++ start/view.ts
@@ -10,7 +10,16 @@
 
 import View from '@ioc:Adonis/Core/View'
 import jrmc from '@jrmc/edge-components'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as akarIcons } from '@iconify-json/akar-icons'
+import { icons as faIcons } from '@iconify-json/fa'
+import { icons as iconoirIcons } from '@iconify-json/iconoir'
+
+addCollection(akarIcons)
+addCollection(faIcons)
+addCollection(iconoirIcons)
 
 View.use(jrmc)
+View.use(edgeIconify)
 
 View.getRenderer()
