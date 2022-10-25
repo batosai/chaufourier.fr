dfd7b0bb8a91dee662e052787c37db4ac9a8a6c3
jeremy <jeremy@chaufourier.fr>
2022-10-26 00:29:42 +0200

    Add buttonSelect component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,12 @@ import '../css/app.css'
 import Alpine from 'alpinejs'
 
 Alpine.start()
+
+
+
+window.getTopPosition = el => el.offsetTop + (el.offsetParent && getTopPosition(el.offsetParent))
+window.getLeftPosition = el => el.offsetLeft + (el.offsetParent && getLeftPosition(el.offsetParent))
+
+window.getBottomPosition = el => getTopPosition(el) + el.offsetHeight
+window.getRightPosition = el => getLeftPosition(el) + el.offsetWidth
+
