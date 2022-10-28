513d903802df1616a5a95faf442969f9a54f6c06
jeremy <jeremy@chaufourier.fr>
2022-10-29 01:32:11 +0200

    WIP filter User

--- app/Controllers/Http/admin/UsersController.ts
+++ app/Controllers/Http/admin/UsersController.ts
@@ -3,11 +3,29 @@ import Route from '@ioc:Adonis/Core/Route'
 import User from 'App/Models/User'
 
 export default class DashboardController {
-  public async index({ request, view, bouncer }: HttpContextContract) {
+  public async index({ request, session, view, bouncer }: HttpContextContract) {
     await bouncer.with('UserPolicy').authorize('viewList')
 
-    const { page = 1, ...input } = request.qs()
+    let { page = 1, reset,  ...input } = request.qs()
     const limit = 10
+
+    if(reset) {
+      if (reset === 'all') {
+        input = {}
+        session.forget('usersFilter')
+      } else {
+        delete input[reset]
+        if (session.has('usersFilter')) {
+          session.forget(`usersFilter.${reset}`)
+        }
+      }
+    }
+
+    if (!Object.keys(input).length && session.has('usersFilter')) {
+      input = session.get('usersFilter')
+    } else if(Object.keys(input).length) {
+      session.put('usersFilter', input)
+    }
 
     const users = await User.filter(input).paginate(page, limit)
     users.baseUrl(Route.builder().make('admin.users.index'))
