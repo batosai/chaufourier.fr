0f03f58da409437c3d909eec1345a2bcbcf43adb
jeremy <jeremy@chaufourier.fr>
2022-10-30 23:35:40 +0100

    Update events.ts

--- start/events.ts
+++ start/events.ts
@@ -11,6 +11,7 @@ import Event from '@ioc:Adonis/Core/Event'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Logger from '@ioc:Adonis/Core/Logger'
 import Application from '@ioc:Adonis/Core/Application'
+import I18n from '@ioc:Adonis/Addons/I18n'
 
 Event.on('db:query', (query) => {
   if (Application.inProduction) {
@@ -19,3 +20,5 @@ Event.on('db:query', (query) => {
     Database.prettyPrint(query)
   }
 })
+
+Event.on('i18n:missing:translation', I18n.prettyPrint)
