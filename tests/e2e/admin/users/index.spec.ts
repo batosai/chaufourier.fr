325163e180a820f845c939534c0e000519118575
jeremy <jeremy@chaufourier.fr>
2022-11-16 22:39:06 +0100

    Update index.spec.ts

--- tests/e2e/admin/users/index.spec.ts
+++ tests/e2e/admin/users/index.spec.ts
@@ -1,27 +1,28 @@
 import { test } from '@japa/runner'
 import Database from '@ioc:Adonis/Lucid/Database'
 import UserFactory from 'Database/factories/UserFactory'
-import Role from 'App/Enums/Roles'
+import Roles from 'App/Enums/Roles'
 import { sleep } from '../../../helpers'
 
 test.group('Admin users', group => {
   group.each.setup(async () => {
     await Database.beginGlobalTransaction()
-    return () => Database.rollbackGlobalTransaction()
+    return async () => {
+      await Database.rollbackGlobalTransaction()
+    }
   })
 
   test('should access users list', async ({ assert, login, page,  route }) => {
     const user = await UserFactory
-      .merge({ password: 'secret' })
+      .merge({ password: 'secret', email: 'virk@adonisjs.com', roleId: Roles.ADMIN })
       .create()
-    user.assignRole(Role.ADMIN)
 
     await UserFactory.createMany(12)
 
     await login(user.email, 'secret')
 
     await page.goto(route('admin.users.index'))
-    // await page.pause();
+    // await page.pause()
 
     assert.lengthOf((await page.$$('tbody tr')), 10)
 
@@ -30,14 +31,13 @@ test.group('Admin users', group => {
         page: 2
       }
     }))
-    assert.lengthOf((await page.$$('tbody tr')), 5)
+    assert.lengthOf(await page.$$('tbody tr'), 3)
   })
 
   test('should find specific user', async ({ assert, login, page,  route }) => {
     const user = await UserFactory
-    .merge({ password: 'secret' })
+    .merge({ password: 'secret', email: 'virk@adonisjs.com', roleId: Roles.ADMIN })
     .create()
-    user.assignRole(Role.ADMIN)
 
     await UserFactory.createMany(12)
 
@@ -48,11 +48,10 @@ test.group('Admin users', group => {
     await page.keyboard.press('Enter');
     await sleep(500)
 
-    assert.lengthOf((await page.$$('tbody tr')), 1)
+    assert.lengthOf(await page.$$('tbody tr'), 1)
   })
 })
 // TODO
-// test ok, WTF error ?????
-// test connexion member, no access
-// replace getscreen and remove package playwright-testing-library
+// test connexion member, no access (dir auth)
 // test filter
+// test reset filter
