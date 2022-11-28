0f7023fb9b95346a70e9e10341c422ac900b4a01
jeremy <jeremy@chaufourier.fr>
2022-11-28 22:30:36 +0100

    Create user

--- app/Controllers/Http/admin/UsersController.ts
+++ app/Controllers/Http/admin/UsersController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
 import User from 'App/Models/User'
 import UserSessionFilterService from 'App/Services/UsersSessionFilterService'
+import UserValidator from 'App/Validators/admin/UserValidator'
 
 export default class DashboardController {
   public async index(ctx: HttpContextContract) {
@@ -11,12 +12,41 @@ export default class DashboardController {
     const limit = 10
     const { page = 1, ...payload } = await UserSessionFilterService.handle(ctx)
 
-    const users = await User.filter(payload).preload('role').paginate(page, limit)
+    const users = await User.filter(payload).paginate(page, limit)
     users.baseUrl(Route.builder().make('admin.users.index'))
     users.queryString(payload)
 
     return view.render('admin/users/index', {
       users,
     })
+  }
+
+  public async create({ view, bouncer }: HttpContextContract) {
+    await bouncer.with('UserPolicy').authorize('create')
+    return view.render('admin/users/create')
+  }
+
+  public async store({ request, response, bouncer }: HttpContextContract) {
+    await bouncer.with('UserPolicy').authorize('create')
+    // const avatar = request.file('avatar')!
+    // /**
+    //  * Validate new user account creation form
+    //  */
+    const payload = await request.validate(UserValidator)
+
+    // /**
+    //  * Create a new user
+    //  */
+    const user = new User()
+    await user.fill(payload)
+
+    // if (avatar) {
+    //   user.avatar = Attachment.fromFile(avatar)
+    // }
+    await user.save()
+
+    // await new SignupMailer(user).sendLater()
+
+    response.redirect().toRoute('admin.users.index')
   }
 }
