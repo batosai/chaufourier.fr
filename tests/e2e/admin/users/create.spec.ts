60c9c8afb247b530166d8182cac754b63c41c924
jeremy <jeremy@chaufourier.fr>
2022-12-06 23:19:47 +0100

    Update create.spec.ts

--- tests/e2e/admin/users/create.spec.ts
+++ tests/e2e/admin/users/create.spec.ts
@@ -3,6 +3,7 @@ import Database from '@ioc:Adonis/Lucid/Database'
 import UserFactory from 'Database/factories/UserFactory'
 import Roles from 'App/Enums/Roles'
 import I18n from '@ioc:Adonis/Addons/I18n'
+import { faker } from '@faker-js/faker'
 
 test.group('Admin create user', (group) => {
   group.each.setup(async () => {
@@ -12,7 +13,7 @@ test.group('Admin create user', (group) => {
     }
   })
 
-  test('should create errors validation required fields', async ({ assert, login, page, route }) => {
+  test('should create errors validation fields', async ({ assert, login, page, route }) => {
     const user = await UserFactory.merge({
       password: 'secret',
       email: 'virk@adonisjs.com',
@@ -26,7 +27,6 @@ test.group('Admin create user', (group) => {
     await page.locator('#lastname').fill('a')
     await page.locator('#firstname').fill('a')
     await page.locator('#email').fill('a')
-
     await page.locator('text=validate').click()
 
     assert.equal(
@@ -46,5 +46,54 @@ test.group('Admin create user', (group) => {
       1
     )
 
+    await page.locator('#lastname').fill(faker.lorem.words(51))
+    await page.locator('#firstname').fill(faker.lorem.words(51))
+    await page.locator('#email').fill('virk@adonisjs.com')
+    await page.locator('text=validate').click()
+
+    assert.equal(
+      await page.getByText(I18n.locale(I18n.defaultLocale).formatMessage('validator.shared.lastname.maxLength', {
+        max_length: 50,
+      })).count(),
+      1
+    )
+    assert.equal(
+      await page.getByText(I18n.locale(I18n.defaultLocale).formatMessage('validator.shared.firstname.maxLength', {
+        max_length: 50,
+      })).count(),
+      1
+    )
+    assert.equal(
+      await page.getByText(I18n.locale(I18n.defaultLocale).formatMessage('validator.shared.email.unique')).count(),
+      1
+    )
+
+  })
+
+  test('should create success', async ({ assert, login, page, route }) => {
+    const user = await UserFactory.merge({
+      password: 'secret',
+      email: 'virk@adonisjs.com',
+      role: Roles.ADMIN,
+    }).create()
+
+    await login(user.email, 'secret')
+
+    await page.goto(route('admin.users.index'))
+
+    assert.equal(await page.locator('tbody tr').count(), 1)
+
+    await page.goto(route('admin.users.create'))
+
+    await page.locator('#lastname').fill(faker.name.lastName())
+    await page.locator('#firstname').fill(faker.name.firstName())
+    await page.locator('#email').fill(faker.internet.email())
+    await page.locator('text=validate').click()
+
+    assert.equal(await page.locator('tbody tr').count(), 2)
+    assert.equal(
+      await page.getByText(I18n.locale(I18n.defaultLocale).formatMessage('form.success.user.create')).count(),
+      1
+    )
   })
 })
