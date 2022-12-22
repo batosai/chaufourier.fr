f5ad1d2db283ecab7ac5452a055b4416b0651b11
jeremy <jeremy@chaufourier.fr>
2022-12-23 00:02:27 +0100

    Create forgot.spec.ts

---
+++ tests/e2e/admin/users/forgot.spec.ts
@@ -0,0 +1,34 @@
+import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+import UserFactory from 'Database/factories/UserFactory'
+import Roles from 'App/Enums/Roles'
+import I18n from '@ioc:Adonis/Addons/I18n'
+
+test.group('Admin forgot password user', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return async () => {
+      await Database.rollbackGlobalTransaction()
+    }
+  })
+
+  test('should send with success', async ({ assert, login, page, route }) => {
+    const user = await UserFactory.merge({
+      password: 'secret',
+      email: 'virk@adonisjs.com',
+      role: Roles.ADMIN,
+    }).create()
+    const customer = await UserFactory.create()
+
+    await login(user.email, 'secret')
+
+    await page.goto(route('admin.users.edit', customer))
+
+    await page.locator('#actions-form').getByRole('button').nth(2).click()
+    await page.getByRole('button', { name: 'ok' }).click()
+    assert.equal(
+      await page.getByText(I18n.locale(I18n.defaultLocale).formatMessage('form.success.user.forgot')).count(),
+      1
+    )
+  })
+})
