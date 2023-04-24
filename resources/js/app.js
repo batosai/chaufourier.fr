09ef24e3c0a803464323530c78edf4dc597ceb5c
jeremy <jeremy@chaufourier.fr>
2023-04-24 14:06:19 +0200

    Update config unpoly

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,14 @@ import '../css/app.css'
 
 import Alpine from 'alpinejs'
 
+up.log.enable()
+
 up.fragment.config.mainTargets.push('.layout-content')
 up.link.config.followSelectors.push('a[href]')
+up.form.config.submitSelectors.push(['form'])
+
+up.layer.config.drawer.size = 'large'
+up.layer.config.drawer.position = 'right'
 
 Alpine.start()
 
