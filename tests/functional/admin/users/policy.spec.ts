259c7db16ecc39539f8c941d38634214bc9a4d3e
jeremy <jeremy@chaufourier.fr>
2023-05-12 00:15:12 +0200

    Create policy.spec.ts

---
+++ tests/functional/admin/users/policy.spec.ts
@@ -0,0 +1,169 @@
+import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+import UserFactory from 'Database/factories/UserFactory'
+import Roles from 'App/Enums/Roles'
+import User from 'App/Models/User'
+
+
+test.group('user policy actions', (group) => {
+  let user: User | null  = null
+  let admin: User | null  = null
+
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+
+    admin = await UserFactory.merge({
+      role: Roles.ADMIN,
+    }).create()
+
+    user = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('A admin can not auto delete', async ({ client, route }) => {
+    const response = await client
+      .delete(route('admin.users.destroy', admin!))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(403)
+  })
+
+  test('A admin can not auto disabled', async ({ client, route }) => {
+    const response = await client
+      .patch(route('admin.users.toggle.disabled', admin!))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(403)
+  })
+
+  test('A admin can not auto forgot password', async ({ client, route }) => {
+    const response = await client
+      .post(route('admin.users.forgot.password', admin!))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(403)
+  })
+
+  test('A admin can not auto impersonate', async ({ client, route }) => {
+    const response = await client
+      .post(route('impersonate.store', admin!))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(403)
+  })
+
+  // -----------------------
+
+  test('A admin can delete user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .delete(route('admin.users.destroy', customer))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(200)
+  })
+
+  test('A admin can disabled user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .patch(route('admin.users.toggle.disabled', customer))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(200)
+  })
+
+  test('A admin can forgot password user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .post(route('admin.users.forgot.password', customer))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(200)
+  })
+
+  test('A admin can impersonate user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .post(route('impersonate.store', customer))
+      .withCsrfToken()
+      .loginAs(admin!)
+
+      response.assertStatus(200)
+  })
+
+  // -----------------------
+
+  test('A user can not delete user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .delete(route('admin.users.destroy', customer))
+      .withCsrfToken()
+      .loginAs(user!)
+
+      response.assertStatus(403)
+  })
+
+  test('A user can not disabled user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .patch(route('admin.users.toggle.disabled', customer))
+      .withCsrfToken()
+      .loginAs(user!)
+
+      response.assertStatus(403)
+  })
+
+  test('A user can not forgot password user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .post(route('admin.users.forgot.password', customer))
+      .withCsrfToken()
+      .loginAs(user!)
+
+      response.assertStatus(403)
+  })
+
+  test('A user can not impersonate user', async ({ client, route }) => {
+    const customer = await UserFactory.merge({
+      role: Roles.USER,
+    }).create()
+
+    const response = await client
+      .post(route('impersonate.store', customer))
+      .withCsrfToken()
+      .loginAs(user!)
+
+      response.assertStatus(403)
+  })
+})
