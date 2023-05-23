7f39540969b98fe6bcd2c54dde993a3dd4a0a34d
jeremy <jeremy@chaufourier.fr>
2023-05-23 23:57:03 +0200

    Create scaffold command

---
+++ commands/MakeScaffold.ts
@@ -0,0 +1,68 @@
+import execa from 'execa'
+import { BaseCommand, args } from '@adonisjs/core/build/standalone'
+
+export default class MakeScaffold extends BaseCommand {
+  /**
+   * Command name is used to run the command
+   */
+  public static commandName = 'make:scaffold'
+
+  @args.string({ description: 'Name of the controller and model class' })
+  public name: string
+
+  /**
+   * Command description is displayed in the "help" output
+   */
+  public static description =
+    'Make a new HTTP controller / Lucid model / view template'
+
+  public static settings = {
+    /**
+     * Set the following value to true, if you want to load the application
+     * before running the command
+     */
+    loadApp: false,
+
+    /**
+     * Set the following value to true, if you want this command to keep running until
+     * you manually decide to exit the process
+     */
+    stayAlive: false,
+  }
+
+  public async run() {
+    await execa.node('ace', ['make:migration', this.name], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:controller', this.name], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:filter', `${this.name}Filter`], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:model', this.name], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:view', `${this.name}/index.edge`], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:view', `${this.name}/create.edge`], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:view', `${this.name}/edit.edge`], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:view', `${this.name}/partials/form.edge`], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:view', `${this.name}/partials/filters.edge`], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:policy', this.name], {
+      stdio: 'inherit',
+    })
+    await execa.node('ace', ['make:validator', `${this.name}Validator`], {
+      stdio: 'inherit',
+    })
+  }
+}
