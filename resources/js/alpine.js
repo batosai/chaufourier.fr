56ea218c36c6ec9ed8f026f524fd77aacd952e61
jeremy <jeremy@chaufourier.fr>
2023-09-09 00:16:17 +0200

    Update health

--- resources/js/alpine.js
+++ resources/js/alpine.js
@@ -12,9 +12,9 @@ Alpine.data('autocomplete', autocomplete)
 
 Alpine.start()
 
-window.getTopPosition = (el) => el.offsetTop + (el.offsetParent && getTopPosition(el.offsetParent))
-window.getLeftPosition = (el) =>
-  el.offsetLeft + (el.offsetParent && getLeftPosition(el.offsetParent))
+// window.getTopPosition = (el) => el.offsetTop + (el.offsetParent && getTopPosition(el.offsetParent))
+// window.getLeftPosition = (el) =>
+//   el.offsetLeft + (el.offsetParent && getLeftPosition(el.offsetParent))
 
-window.getBottomPosition = (el) => getTopPosition(el) + el.offsetHeight
-window.getRightPosition = (el) => getLeftPosition(el) + el.offsetWidth
+// window.getBottomPosition = (el) => getTopPosition(el) + el.offsetHeight
+// window.getRightPosition = (el) => getLeftPosition(el) + el.offsetWidth
