9c5b09c4eac7901f69bc9d222e0ddcc251a6e67c
jeremy <jeremy@chaufourier.fr>
2023-11-16 23:13:54 +0100

    Auto close after upload

--- resources/modules/alpinejs/uppy.js
+++ resources/modules/alpinejs/uppy.js
@@ -1,11 +1,15 @@
 import Uppy from '@uppy/core'
 import Dashboard from '@uppy/dashboard'
+// import DragDrop from '@uppy/drag-drop'
+// import StatusBar from '@uppy/status-bar'
 import ImageEditor from '@uppy/image-editor'
 import XHR from '@uppy/xhr-upload'
 
 import '@uppy/core/dist/style.min.css'
 import '@uppy/dashboard/dist/style.min.css'
 import '@uppy/image-editor/dist/style.min.css'
+// import '@uppy/drag-drop/dist/style.min.css'
+// import '@uppy/status-bar/dist/style.min.css'
 
 export default () => ({
   uppy: null,
@@ -13,6 +17,12 @@ export default () => ({
   init() {
     const csrf = document.querySelector('meta[name="csrf-token"]').content
     this.uppy = new Uppy()
+    // .use(DragDrop, { target: this.$el })
+    // .use(StatusBar, {
+    //   target: this.$el,
+    //   hideAfterFinish: false,
+    //   showProgressDetails: true
+    // })
       .use(Dashboard, {
         inline: true,
         width: '100%',
@@ -33,6 +43,17 @@ export default () => ({
         headers: () => ({
           'X-CSRF-TOKEN': csrf,
         }),
+      })
+
+      this.uppy.on("complete", (result) => {
+        if (result.failed.length === 0) {
+          // console.log("Upload successful üòÄ")
+          up.layer.current.dismiss()
+        } else {
+          // console.warn("Upload failed üòû")
+        }
+        // console.log("successful files:", result.successful)
+        // console.log("failed files:", result.failed)
       })
   },
 })
