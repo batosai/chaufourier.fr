8e49adbf92cd9671d1a8130933d81e44557bfc87
jeremy <jeremy@chaufourier.fr>
2023-12-20 23:30:17 +0100

    Update admin.js

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,4 +1,10 @@
 import './admin/unpoly'
 import './admin/alpine'
+import hljs from 'highlight.js'
 
 import '../css/admin.css'
+import 'highlight.js/styles/atom-one-dark.min.css'
+
+window.hljs = hljs
+hljs.initHighlightingOnLoad()
+
