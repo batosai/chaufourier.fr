870beb2573e293c01ca42198a448c0a297a534d0
jeremy <jeremy@chaufourier.fr>
2024-01-10 00:18:38 +0100

    WIP theme

--- tailwind.front.config.js
+++ tailwind.front.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const edgeComponent = require('@jrmc/edge-components/build/tailwind.config').default
-const light = require('daisyui/src/theming/themes')['[data-theme=light]']
-const dark = require('daisyui/src/theming/themes')['[data-theme=dark]']
+// const light = require('daisyui/src/theming/themes')['[data-theme=light]']
+// const dark = require('daisyui/src/theming/themes')['[data-theme=dark]']
 
 module.exports = {
   content: [
@@ -10,206 +10,191 @@ module.exports = {
     './resources/views/front/**/*.edge',
     './resources/views/components/front/**/*.edge',
   ],
-  safelist: ['dark'],
-  theme: {
-    fontFamily: {
-      body: ['Poppins', 'sans-serif'],
-    },
+  // safelist: ['dark'],
+  // theme: {
+  //   fontFamily: {
+  //     body: ['Poppins', 'sans-serif'],
+  //   },
 
-    screens: {
-      xs: '375px',
-      ...defaultTheme.screens,
-    },
+  //   screens: {
+  //     xs: '375px',
+  //     ...defaultTheme.screens,
+  //   },
 
-    colors: {
-      'transparent': 'transparent',
-      'primary': '#072344',
-      'secondary': '#00aaa1',
-      'green-light': '#cceeec',
-      'green': '#007c85',
-      'green-dark': '#065a68',
-      'blue-light': '#b3d6f1',
-      'blue': '#0074d1',
-      'blue-dark': '#072344',
-      'black': '#000000',
-      'white': '#ffffff',
-      'yellow-lighter': '#f6e8c6',
-      'yellow-light': '#f8edd0',
-      'yellow': '#f4d06f',
-      'yellow-dark': '#daa512',
-      'grey-lightest': '#eff0f3',
-      'grey-lighter': '#eceef1',
-      'grey-light': '#ccd7e0',
-      'grey': '#adb6c4',
-      'red-light': '#D93526',
-      'red-dark': '#861D13',
-      'red': '#D93526',
-    },
+  //   colors: {
+  //     'transparent': 'transparent',
+  //     'primary': '#072344',
+  //     'secondary': '#00aaa1',
+  //     'green-light': '#cceeec',
+  //     'green': '#007c85',
+  //     'green-dark': '#065a68',
+  //     'blue-light': '#b3d6f1',
+  //     'blue': '#0074d1',
+  //     'blue-dark': '#072344',
+  //     'black': '#000000',
+  //     'white': '#ffffff',
+  //     'yellow-lighter': '#f6e8c6',
+  //     'yellow-light': '#f8edd0',
+  //     'yellow': '#f4d06f',
+  //     'yellow-dark': '#daa512',
+  //     'grey-lightest': '#eff0f3',
+  //     'grey-lighter': '#eceef1',
+  //     'grey-light': '#ccd7e0',
+  //     'grey': '#adb6c4',
+  //     'red-light': '#D93526',
+  //     'red-dark': '#861D13',
+  //     'red': '#D93526',
+  //   },
 
-    border: {
-      DEFAULT: '1px',
-      0: '0',
-      2: '2px',
-      4: '4px',
-      6: '6px',
-      8: '8px',
-    },
+  //   border: {
+  //     DEFAULT: '1px',
+  //     0: '0',
+  //     2: '2px',
+  //     4: '4px',
+  //     6: '6px',
+  //     8: '8px',
+  //   },
 
-    container: {
-      center: true,
-      padding: '1rem',
-    },
+  //   container: {
+  //     center: true,
+  //     padding: '1rem',
+  //   },
 
-    extend: {
-      spacing: {
-        13: '3.25rem',
-        15: '3.75rem',
-        17: '4.25rem',
-        18: '4.5rem',
-        19: '4.75rem',
-        76: '19rem',
-        84: '21rem',
-        88: '22rem',
-        92: '23rem',
-        100: '25rem',
-        104: '26rem',
-        108: '27rem',
-        112: '28rem',
-        116: '29rem',
-        120: '30rem',
-        124: '31rem',
-        128: '32rem',
-        132: '33rem',
-        136: '34rem',
-        140: '35rem',
-        144: '36rem',
-        148: '37rem',
-        152: '38rem',
-        156: '39rem',
-        160: '40rem',
-        164: '41rem',
-        168: '42rem',
-        172: '43rem',
-        176: '44rem',
-        180: '45rem',
-        184: '46rem',
-        188: '47rem',
-        190: '48rem',
-        194: '49rem',
-        200: '50rem',
-        204: '51rem',
-      },
-      inset: {
-        50: '50%',
-        100: '100%',
-      },
-      zIndex: {
-        '-1': '-1',
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            'color': theme('colors.primary'),
-            'a': {
-              'fontWeight': theme('fontWeight.semibold'),
-              'color': theme('colors.green'),
-              'textDecoration': 'underline',
-              'transition': 'color 300ms',
-              '&:hover': {
-                color: theme('colors.primary'),
-              },
-            },
-            'p, li': {
-              fontWeight: theme('fontWeight.light'),
-            },
-            'h1, h2, h3, h4, h5, h6': {
-              fontWeight: theme('fontWeight.semibold'),
-            },
-            'ul > li::before': {
-              backgroundColor: theme('colors.primary'),
-            },
-            'blockquote': {
-              borderLeftWidth: '1rem',
-              borderColor: theme('colors.green-dark'),
-              borderRadius: '3px',
-              backgroundColor: theme('colors.green-light'),
-              padding: `${theme('spacing.4')} ${theme('spacing.6')}`,
-              color: theme('colors.green'),
-              fontStyle: 'normal',
-              p: {
-                margin: 0,
-                fontWeight: theme('fontWeight.normal'),
-              },
-            },
-            '.alert': {
-              borderLeftWidth: '1rem',
-              borderColor: theme('colors.green-dark'),
-              borderRadius: '3px',
-              backgroundColor: theme('colors.green-light'),
-              padding: `${theme('spacing.4')} ${theme('spacing.6')}`,
-              color: theme('colors.green'),
-              fontStyle: 'normal',
-              margin: '1.25em 0',
-              p: {
-                margin: 0,
-                fontWeight: theme('fontWeight.normal'),
-              },
-            },
-            '.alert-info': {
-              borderColor: theme('colors.blue'),
-              backgroundColor: theme('colors.blue-light'),
-              color: theme('colors.blue'),
-            },
-            '.alert-warning': {
-              borderColor: theme('colors.yellow-dark'),
-              backgroundColor: theme('colors.yellow-light'),
-              color: theme('colors.grey'),
-            },
-            '.alert-error': {
-              borderColor: theme('colors.red-dark'),
-              backgroundColor: theme('colors.red-light'),
-              color: theme('colors.white'),
-            },
-          },
-        },
-        dark: {
-          css: {
-            'color': theme('colors.white'),
-            'a': {
-              'color': theme('colors.secondary'),
-              '&:hover': {
-                color: theme('colors.green'),
-              },
-            },
-            'h1, h2, h3, h4, h5, h6': {
-              color: theme('colors.white'),
-            },
-            'ul > li::before': {
-              backgroundColor: theme('colors.secondary'),
-            },
-          },
-        },
-      }),
-    },
-  },
+  //   extend: {
+  //     spacing: {
+  //       13: '3.25rem',
+  //       15: '3.75rem',
+  //       17: '4.25rem',
+  //       18: '4.5rem',
+  //       19: '4.75rem',
+  //       76: '19rem',
+  //       84: '21rem',
+  //       88: '22rem',
+  //       92: '23rem',
+  //       100: '25rem',
+  //       104: '26rem',
+  //       108: '27rem',
+  //       112: '28rem',
+  //       116: '29rem',
+  //       120: '30rem',
+  //       124: '31rem',
+  //       128: '32rem',
+  //       132: '33rem',
+  //       136: '34rem',
+  //       140: '35rem',
+  //       144: '36rem',
+  //       148: '37rem',
+  //       152: '38rem',
+  //       156: '39rem',
+  //       160: '40rem',
+  //       164: '41rem',
+  //       168: '42rem',
+  //       172: '43rem',
+  //       176: '44rem',
+  //       180: '45rem',
+  //       184: '46rem',
+  //       188: '47rem',
+  //       190: '48rem',
+  //       194: '49rem',
+  //       200: '50rem',
+  //       204: '51rem',
+  //     },
+  //     inset: {
+  //       50: '50%',
+  //       100: '100%',
+  //     },
+  //     zIndex: {
+  //       '-1': '-1',
+  //     },
+  //     typography: (theme) => ({
+  //       DEFAULT: {
+  //         css: {
+  //           'color': theme('colors.primary'),
+  //           'a': {
+  //             'fontWeight': theme('fontWeight.semibold'),
+  //             'color': theme('colors.green'),
+  //             'textDecoration': 'underline',
+  //             'transition': 'color 300ms',
+  //             '&:hover': {
+  //               color: theme('colors.primary'),
+  //             },
+  //           },
+  //           'p, li': {
+  //             fontWeight: theme('fontWeight.light'),
+  //           },
+  //           'h1, h2, h3, h4, h5, h6': {
+  //             fontWeight: theme('fontWeight.semibold'),
+  //           },
+  //           'ul > li::before': {
+  //             backgroundColor: theme('colors.primary'),
+  //           },
+  //           'blockquote': {
+  //             borderLeftWidth: '1rem',
+  //             borderColor: theme('colors.green-dark'),
+  //             borderRadius: '3px',
+  //             backgroundColor: theme('colors.green-light'),
+  //             padding: `${theme('spacing.4')} ${theme('spacing.6')}`,
+  //             color: theme('colors.green'),
+  //             fontStyle: 'normal',
+  //             p: {
+  //               margin: 0,
+  //               fontWeight: theme('fontWeight.normal'),
+  //             },
+  //           },
+  //           '.alert': {
+  //             borderLeftWidth: '1rem',
+  //             borderColor: theme('colors.green-dark'),
+  //             borderRadius: '3px',
+  //             backgroundColor: theme('colors.green-light'),
+  //             padding: `${theme('spacing.4')} ${theme('spacing.6')}`,
+  //             color: theme('colors.green'),
+  //             fontStyle: 'normal',
+  //             margin: '1.25em 0',
+  //             p: {
+  //               margin: 0,
+  //               fontWeight: theme('fontWeight.normal'),
+  //             },
+  //           },
+  //           '.alert-info': {
+  //             borderColor: theme('colors.blue'),
+  //             backgroundColor: theme('colors.blue-light'),
+  //             color: theme('colors.blue'),
+  //           },
+  //           '.alert-warning': {
+  //             borderColor: theme('colors.yellow-dark'),
+  //             backgroundColor: theme('colors.yellow-light'),
+  //             color: theme('colors.grey'),
+  //           },
+  //           '.alert-error': {
+  //             borderColor: theme('colors.red-dark'),
+  //             backgroundColor: theme('colors.red-light'),
+  //             color: theme('colors.white'),
+  //           },
+  //         },
+  //       },
+  //       dark: {
+  //         css: {
+  //           'color': theme('colors.white'),
+  //           'a': {
+  //             'color': theme('colors.secondary'),
+  //             '&:hover': {
+  //               color: theme('colors.green'),
+  //             },
+  //           },
+  //           'h1, h2, h3, h4, h5, h6': {
+  //             color: theme('colors.white'),
+  //           },
+  //           'ul > li::before': {
+  //             backgroundColor: theme('colors.secondary'),
+  //           },
+  //         },
+  //       },
+  //     }),
+  //   },
+  // },
   daisyui: {
-    themes: [
-      {
-        light: {
-          ...light,
-          primary: light.accent,
-          accent: light.primary,
-        },
-      },
-      {
-        dark: {
-          ...dark,
-          primary: dark.accent,
-          accent: dark.primary,
-        },
-      },
-    ],
-    darkTheme: 'dark',
+    themes: [ 'emerald', 'dracula' ],
+    darkTheme: 'dracula',
   },
   plugins: [
     require('@tailwindcss/typography')({
