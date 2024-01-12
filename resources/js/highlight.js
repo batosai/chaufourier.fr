f4e403b8e333df2dc0766b172f59a066de95ca31
jeremy <jeremy@chaufourier.fr>
2024-01-12 19:13:26 +0100

    Add global highlight.js

--- resources/js/highlight.js
+++ resources/js/highlight.js
@@ -1,5 +1,9 @@
 import hljs from 'highlight.js'
 
-hljs.initHighlightingOnLoad()
+hljs.configure({ languages: ['js', 'html', 'css', 'ruby', 'erb', 'bash', 'markdown', 'dockerfile', 'json',  'php', 'python', 'rust', 'sql', 'nginx'] })
+
+document.body.addEventListener('htmx:load', function() {
+  hljs.highlightAll()
+})
 
 import 'highlight.js/styles/atom-one-dark.min.css'
