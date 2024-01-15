f6e559f2ff15a724bb447ca0fba9642c14b02a04
jeremy <jeremy@chaufourier.fr>
2024-01-16 00:15:14 +0100

    Add redirects seed

--- database/seeders/MainSeeder/Index.ts
+++ database/seeders/MainSeeder/Index.ts
@@ -6,6 +6,7 @@ export default class extends BaseSeeder {
     await this.runSeeder(await import('../development/User'))
     await this.runSeeder(await import('../Tag'))
     await this.runSeeder(await import('../Article'))
+    await this.runSeeder(await import('../Redirect'))
   }
 
   private async runSeeder(Seeder: { default: typeof BaseSeeder }) {
