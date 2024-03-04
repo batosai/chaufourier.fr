6bee8811454ce41b65d86b8ffd30947aaa868589
jeremy <jeremy@chaufourier.fr>
2024-03-04 22:44:44 +0100

    Fix aspect ratio video youtube

--- tailwind.front.config.js
+++ tailwind.front.config.js
@@ -11,6 +11,7 @@ module.exports = {
   ],
   plugins: [
     require('tailwindcss-safe-area'),
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography')({
       modifiers: [],
     }),
