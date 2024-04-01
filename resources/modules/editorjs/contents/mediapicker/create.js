89a296202de7e72005411d3b3332c93a3cfaa833
jeremy <jeremy@chaufourier.fr>
2024-04-01 17:52:02 +0200

    add webp in media list

--- resources/modules/editorjs/contents/mediapicker/create.js
+++ resources/modules/editorjs/contents/mediapicker/create.js
@@ -7,7 +7,7 @@ export default ({ target }) => `<div class="flex items-center gap-x-8 ${target}"
   <input type="hidden" class="input" name="${target}" id="${target}" value="">
 
   <div>
-    <a role="button" href="/admin/mediapicker?target=${target}&amp;entity=articles&amp;types%5B0%5D=image%2Fjpeg&amp;types%5B1%5D=image%2Fpng&amp;name=${target}" up-layer="new" up-mode="drawer" up-size="grow" up-accept-location="/admin/mediapicker/*" up-on-accepted="up.render('.${target}', { response: event.response })" up-history="false" class="btn btn-primary btn-sm normal-case">
+    <a role="button" href="/admin/mediapicker?target=${target}&amp;entity=articles&amp;types%5B0%5D=image%2Fjpeg&amp;types%5B1%5D=image%2Fpng&amp;types%5B2%5D=image%2Fwebp&amp;name=${target}" up-layer="new" up-mode="drawer" up-size="grow" up-accept-location="/admin/mediapicker/*" up-on-accepted="up.render('.${target}', { response: event.response })" up-history="false" class="btn btn-primary btn-sm normal-case">
       Add media
     </a>
 
--- resources/modules/editorjs/contents/mediapicker/edit.js
+++ resources/modules/editorjs/contents/mediapicker/edit.js
@@ -7,7 +7,7 @@ export default ({ target, img, id }) => `<div class="flex items-center gap-x-8 $
   <input type="hidden" class="input" name="${target}" id="${target}" value="${id}">
 
   <div>
-    <a role="button" href="/admin/mediapicker?target=${target}&amp;entity=articles&amp;types%5B0%5D=image%2Fjpeg&amp;types%5B1%5D=image%2Fpng&amp;name=${target}" up-layer="new" up-mode="drawer" up-size="grow" up-accept-location="/admin/mediapicker/*" up-on-accepted="up.render('.${target}', { response: event.response })" up-history="false" class="btn btn-primary btn-sm normal-case">
+    <a role="button" href="/admin/mediapicker?target=${target}&amp;entity=articles&amp;types%5B0%5D=image%2Fjpeg&amp;types%5B1%5D=image%2Fpng&amp;types%5B2%5D=image%2Fwebp&amp;name=${target}" up-layer="new" up-mode="drawer" up-size="grow" up-accept-location="/admin/mediapicker/*" up-on-accepted="up.render('.${target}', { response: event.response })" up-history="false" class="btn btn-primary btn-sm normal-case">
       Change media
     </a>
     <a role="button" href="/admin/mediapicker/create?target=${target}&amp;entity=articles" up-target=".${target}" class="btn btn-link btn-xs text-error normal-case">
